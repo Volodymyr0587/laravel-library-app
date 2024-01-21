03ed94e130f4d444c1910b3ffca5c386bf3516a1
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-01-21 16:01:21 +0200

    Finalizing the controllers

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('books.index', ['books' => Book::all()]);
+    }
 }
